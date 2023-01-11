9225e976bb7a1d5d5923cb526d8234e0e210a929
harbyanwardi <anwardiharby@gmail.com>
2023-01-11 15:35:47 +0700

    1st

---
+++ listener-service/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"listener-service/event"
+	"log"
+	"math"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	//try to connect rabbitmq
+	rabbitConn, err := connect()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+	defer rabbitConn.Close()
+
+	//start listening rabbitmq
+	log.Println("Listening for consumming rabbitMQ")
+
+	//create consumer
+	consumer, err := event.NewConsumer(rabbitConn)
+	if err != nil {
+		panic(err)
+	}
+
+	//watch the queue and consume event
+	err = consumer.Listen([]string{"log.INFO", "log.WARNING", "log.ERROR"})
+	if err != nil {
+		log.Println(err)
+	}
+
+}
+
+func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
+
+	//dont continue until rabbit is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			fmt.Println("Rabbit not yet ready")
+			counts++
+		} else {
+			log.Println("Connected RabbitMQ")
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off")
+		time.Sleep(backOff)
+		continue
+
+	}
+	return connection, nil
+}
---
+++ logger-service/cmd/api/grpc.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"logger-service/data"
+	"logger-service/logs"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type LogServer struct {
+	logs.UnimplementedLogServiceServer
+	Models data.Models
+}
+
+func (l *LogServer) WriteLog(ctx context.Context, req *logs.LogRequest) (*logs.LogResponse, error) {
+	input := req.GetLogentry()
+
+	//write the log
+	logEntry := data.LogEntry{
+		Name: input.Name,
+		Data: input.Data,
+	}
+
+	err := l.Models.LogEntry.Insert(logEntry)
+	if err != nil {
+		res := &logs.LogResponse{Result: "failed"}
+		return res, err
+	}
+
+	//return response
+	res := &logs.LogResponse{Result: "logged!"}
+	return res, nil
+}
+
+func (app *Config) gRPCListen() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen grpc :  %v", err)
+	}
+	s := grpc.NewServer()
+	logs.RegisterLogServiceServer(s, &LogServer{Models: app.Models})
+	log.Printf("grpc started on port %s", grpcPort)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to listen grpc :  %v", err)
+	}
+
+}
